eda221f256962e1dedf5212ddc04655718553f13
masashi <masashi@cizucu.com>
2024-10-21 17:31:50 +0900

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+
+
+import { useState } from 'react';
+
+export default function Home() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>My ToDo List</h1>
+      <input 
+        type="text" 
+        value={newTodo} 
+        onChange={(e) => setNewTodo(e.target.value)} 
+      />
+      <button onClick={addTodo}>Add ToDo</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
